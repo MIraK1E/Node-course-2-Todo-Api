8b81edf45240eddc9685d23dc7fe7846dc348571
MIraK1E <mike.pattana@gmail.com>
2018-07-19 14:55:00 +0700

    Test POST /todos route

---
+++ server/test/server.test.js
@@ -0,0 +1,58 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const { app } = require('./../server');
+const { Todo } = require('./../model/todo');
+
+// this function will run before test case
+// it will run test case when call done
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+})
+
+describe('POST/todos', () => {
+
+    it('should create a new todo', (done) => {
+        const text = 'test todo text';
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(201)
+            .expect((res) => {
+                expect(res.body.text).toBe(text)
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                Todo.find()
+                    .then((todos) => {
+                        expect(todos.length).toBe(1);
+                        expect(todos[0].text).toBe(text);
+                        done()
+                    })
+                    .catch((err) => done(err));
+            });
+    });
+
+    it('should not create todo with invalid data', (done) => {
+        request(app)
+            .post('/todos')
+            .send()
+            .expect(400)
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                Todo.find()
+                    .then((todos) => {
+                        expect(todos.length).toBe(0);
+                        done();
+                    })
+                    .catch((err) => done(err));
+            })
+    });
+
+});
